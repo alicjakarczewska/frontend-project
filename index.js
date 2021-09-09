ab779f24d66092c58c20c739266e6aa63fbf8eb6
alicjakarczewska <alicja.karczewska1@gmail.com>
2021-09-09 11:32:33 +0200

    splitting the code into files

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ import { externalModule } from './js/myModule';
  * Import your styles!
  */
 import './styles/main.scss';
+import './styles/contact.scss';
+import './styles/faq.scss';
+import './styles/store.scss';
+import './styles/ekosystem.scss';
+import './styles/partners.scss';
+import './styles/bringTo21St.scss';
+import './styles/downloadPanel.scss';
+import './styles/info.scss';
+import './styles/roomBooking.scss';
+import './styles/features.scss';
+import './styles/problemSolving.scss';
+import './styles/products.scss';
+import './styles/header.scss';
+import './styles/mainList.scss';
+import './styles/menu.scss';
+import './styles/footer.scss';
+
+
+
 /**
 * externalModule.greet();
 */
